5c3c262a4aaabfcbb87da86612e09d4b20b2f31f
MikeGogol <yogi.gogol@gmail.com>
2021-11-18 12:22:25 +0100

    changes

--- backend/database.js
+++ backend/database.js
@@ -5,6 +5,8 @@ const databaseUser = process.env.DB_USER;
 const databasePassword = process.env.DB_PASSWORD;
 const databaseHost = process.env.DB_HOST;
 
+console.log("test baza", process.env.TEST_VARIABLE);
+
 const database = new Sequelize(databaseName, databaseUser, databasePassword, {
   host: databaseHost,
   dialect: "mysql",
@@ -19,6 +21,7 @@ const database = new Sequelize(databaseName, databaseUser, databasePassword, {
 async function tryConnection() {
   /* eslint-disable */
   try {
+    console.log("rds", process.env.RDS_USERNAME);
     await database.authenticate();
     console.log("Connection with database has been established successfully.");
   } catch (error) {
